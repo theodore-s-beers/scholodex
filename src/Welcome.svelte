295852ba1a5bc98e880e04791469abb645a84c6f
Theodore Beers <theo.beers@fu-berlin.de>
2020-11-05 13:36:35 +0100

    Confirm that everything still works

--- src/Welcome.svelte
+++ src/Welcome.svelte
@@ -27,15 +27,17 @@
 
 <div class="welcome" in:fade>
   <p>
-    Welcome! This is a prototype for a kind of <em>scholar’s rolodex</em>,
-    developed using the <a href="https://svelte.dev/">Svelte</a> framework (to use
-    the term loosely) with TypeScript.
+    Welcome! This is a prototype for a kind of
+    <em>scholar’s rolodex</em>, developed using the
+    <a href="https://svelte.dev/">Svelte</a>
+    framework (to use the term loosely) with TypeScript.
   </p>
 
   <p>
     Feel free to play around with this, but it isn’t yet fit for serious use.
     There is no back end, and therefore no persistent data storage. Any contact
-    cards that you create will (ideally) be saved as <a
+    cards that you create will (ideally) be saved as
+    <a
       href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">localStorage</a>
     in your web browser.
   </p>
@@ -43,14 +45,13 @@
   <p>
     Using the buttons above, you can add new cards, search in their contents, or
     summon this description again (once there’s something else to display). If
-    you’d like to load a set of sample cards, to get a sense of the interface, <a
-      href="#sample"
-      class="sampleLink">click here</a>.
+    you’d like to load a set of sample cards, to get a sense of the interface,
+    <a href="#sample" class="sampleLink">click here</a>.
   </p>
 
   <p>
     I’m developing this as a way of learning Svelte—the first modern front-end
-    framework that I’ve tried and actually enjoyed. My source code is available <a
-      href="https://github.com/theodore-s-beers/scholodex">on GitHub</a>.
+    framework that I’ve tried and actually enjoyed. My source code is available
+    <a href="https://github.com/theodore-s-beers/scholodex">on GitHub</a>.
   </p>
 </div>
